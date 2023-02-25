87703e20d677f977852c7d651ac6e247a5d922a9
Rivaldo Sanchez Pizango <rivaldosanchezp@gmail.com>
2023-02-25 16:47:08 -0300

    ejercicios de logica

--- EjerciciosDeLogicaDeProgramacion/ejercicio6.js
+++ EjerciciosDeLogicaDeProgramacion/ejercicio6.js
@@ -1,8 +1,8 @@
 //18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
-const comtarLetras = (cadena = "") => {
+const contarLetras = (cadena = "") => {
   if (!cadena) return console.warn("No ingresaste una cadena de texto")
 
-  if (typeof cadena !== "string") return console.error(`El valor ${cadena} ingresado, No es una cadena de texto`)
+  if (typeof cadena !== "string") return console.log(`El valor ${cadena} ingresado, No es una cadena de texto`)
 
   let vocales = 0
   consonantes = 0
@@ -21,7 +21,70 @@ const comtarLetras = (cadena = "") => {
 
   return console.info()
 }
+
+contarLetras("hola mundo")
+
+
+
+
+
 console.log("-------------------------------------------------------")
 //19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+const validarNombre = (nombre = "") => {
+  if (!nombre) return console.log("No ingresaste un nombre")
+
+  if (typeof nombre !== "string") return console.log(`El valor ${nombre} ingresado, No es una cadena de texto`)
+
+
+  let expReg = /^[A-Za-zÑñAaEeIiOoUu\s]+$/g.test(nombre)
+  return (expReg)
+    ? console.info(`${nombre} es un  nombre valido`)
+    : console.log(`${nombre} No es un nombre valido`)
+}
+
+validarNombre()
+validarNombre("rivaldo")
+validarNombre("Rivaldo 19")
+
+
+
+
+
 console.log("-------------------------------------------------------")
 //20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+const validarEmail = (email = "") => {
+  if (!email) return console.log("No ingresaste un nombre")
+
+  if (typeof email !== "string") return console.log(`El valor ${email} ingresado, No es una cadena de texto`)
+
+
+  let expReg = /[a-z0-9]+(\.[a-z0-9]+)*@[a-z0-9-]+(\.[a-z]{2,15})/i.test(email)
+  return (expReg)
+    ? console.info(`${email} es un correo valido`)
+    : console.log(`${email} No es un correo valido`)
+}
+validarEmail()
+validarEmail("osdo")
+validarEmail("rivaldosanchezp@gmail.com")
+
+
+
+
+console.log("-------------------------------------------------------")
+//Fusion 19 y 20
+const validarPatron = (cadena = "", patron = undefined) => {
+  if (!cadena) return console.log("No ingresaste una cadena de texto a evaluar")
+
+  if (typeof cadena !== "string") return console.log(`El valor ${cadena} ingresado, No es una cadena de texto`)
+
+  if (patron === undefined) return console.log("No ingresaste una cadena de texto a evaluar")
+
+  if (!(patron instanceof RegExp)) return console.log(`El valor ${patron} ingresado, No es una expresion regular`)
+
+  let expReg = patron.test(cadena)
+  return (expReg)
+    ? console.info(`${cadena} cumple con el patron ingresado`)
+    : console.log(`${cadena} No cumple con el patron ingresado`)
+}
+
+validarPatron("rivaldo sanchez", /^[A-Za-zÑñAaEeIiOoUu\s]+$/g)
--- EjerciciosDeLogicaDeProgramacion/ejercicio7.js
+++ EjerciciosDeLogicaDeProgramacion/ejercicio7.js
@@ -1,3 +1,70 @@
 //21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+const devolverCuadrados = (arr = undefined) => {
+  if (arr === undefined) return console.log("No ingresaste un arreglo de numeros")
+
+  if (!(arr instanceof Array)) return console.log("El valor que ingresaste no es un arreglo")
+
+  if (arr.length === 0) return console.log("El arreglo esta vacio")
+
+  for (let numero of arr) {
+    if (typeof numero !== 'number') return console.log(`El valor ${numero} ingresado, No es un numero`)
+  }
+
+  const newArr = arr.map(el => el * el)
+
+  return console.log(`Arreglo original ${arr} .\narreglo elevado al cuadrado ${newArr}`)
+}
+
+devolverCuadrados()
+devolverCuadrados({})
+devolverCuadrados([1, 4, 8])
+
+
+
+
+
+
+
+console.log("-------------------------------------------------------")
 //22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+const arrayMinMax = (arr = undefined) => {
+  if (arr === undefined) return console.log("No ingresaste un arreglo de numeros")
+
+  if (!(arr instanceof Array)) return console.log("El valor que ingresaste no es un arreglo")
+
+  if (arr.length === 0) return console.log("El arreglo esta vacio")
+
+  for (let numero of arr) {
+    if (typeof numero !== 'number') return console.log(`El valor ${numero} ingresado, No es un numero`)
+  }
+
+  return console.log(`Arreglo original: ${arr}\n Valor Mayor ${Math.max(...arr)}\n Valor Menor ${Math.min(...arr)}`)
+}
+
+arrayMinMax([1, 2, 34, 556, 67, 57])
+
+
+
+
+
+console.log("-------------------------------------------------------")
 //23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
+const separarParesImpares = (arr = undefined) => {
+  if (arr === undefined) return console.log("No ingresaste un arreglo de numeros")
+
+  if (!(arr instanceof Array)) return console.log("El valor que ingresaste no es un arreglo")
+
+  if (arr.length === 0) return console.log("El arreglo esta vacio")
+
+  for (let numero of arr) {
+    if (typeof numero !== 'number') return console.log(`El valor ${numero} ingresado, No es un numero`)
+  }
+
+  return console.log({
+    pares: arr.filter(num => num % 2 === 0),
+    impares: arr.filter(num => num % 2 === 1)
+  })
+}
+
+separarParesImpares()
+separarParesImpares([1, 2, 34, 556, 67, 57])
